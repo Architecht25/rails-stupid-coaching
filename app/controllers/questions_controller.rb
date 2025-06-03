9ede61e38e8ebaebaa24c37ea96703b834c5574f
ROBIN DU APRC <ROBIN@PRIMES-SERVICES.BE>
2025-06-03 19:50:26 +0200

    much more fun then the previous backend code

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,4 +1,17 @@
 class QuestionsController < ApplicationController
   def ask
   end
+
+  def answer
+    @ask = params[:ask]
+    @answer = if @ask.blank?
+                "Please ask a question!"
+              elsif @ask.downcase == "i am going to work"
+                "Great!"
+              elsif @ask.end_with?("?")
+                "Silly question, get dressed and go to work!"
+              else
+                "I don't care, get dressed and go to work!"
+              end
+  end
 end
